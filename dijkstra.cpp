c62d361151d8274879692981dc2761d21910a972
Maria Azam <mariauazam@gmail.com>
2020-08-19 19:41:40 -0600

    Implementing adjacency matrix

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -6,3 +6,52 @@
     Purpose:        Implement Dijkstra's Algorithm to find the least cost path from one node to all other vertices in a graph.
     References:     
 */
+
+#include <iostream>
+using namespace std;
+
+const int MAX_VERTEX = 100;
+void addEdge(int matrix[][MAX_VERTEX], int s, int d, int w);
+void printGraph(int matrix[][MAX_VERTEX], int n);
+
+int main()
+{
+
+    int matrix[MAX_VERTEX][MAX_VERTEX];
+
+    //Prompt user to enter number of vertices in graph
+    int vertices;
+    cout << "Enter the number of vertices, to a maximum of 100: ";
+    cin >> vertices;
+
+    int edges;
+    cout << "Enter the number of edges: ";
+    cin >> edges;
+    int s, d, w;
+    for (int i = 0; i < edges; i++)
+    {
+        cout << "Enter the source node, destination node, and weight: ";
+        cin >> s >> d >> w;
+        addEdge(matrix, s, d, w);
+    }
+
+    printGraph(matrix, vertices);
+
+    return 0;
+}
+
+
+void addEdge(int matrix[][MAX_VERTEX], int s, int d, int w)
+{
+    matrix[s][d] = w;
+}
+
+void printGraph(int matrix[][MAX_VERTEX], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            cout << matrix[i][j] << " ";
+        cout << endl;
+    }
+}
