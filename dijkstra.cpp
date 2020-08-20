88152b76266ea99f637297a6893865f4ae77a565
Maria Azam <mariauazam@gmail.com>
2020-08-19 19:11:18 -0600

    Create dijkstra.cpp

---
+++ dijkstra.cpp
@@ -0,0 +1,8 @@
+/*
+    Student Name:   Maria Azam
+    Student ID:     200402217
+    Date:           Wednesday, August 19, 2020
+
+    Purpose:        Implement Dijkstra's Algorithm to find the least cost path from one node to all other vertices in a graph.
+    References:     
+*/
